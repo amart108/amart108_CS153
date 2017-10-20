37f23535fe6b22b738738f4d2f3e9f682d8f605d
Antonio Martinez <amart108@sledge.cs.ucr.edu>
2017-10-20 12:17:31 -0700

    third commit

--- xv6/wait_test.c
+++ xv6/wait_test.c
@@ -10,6 +10,15 @@ int main(void)
    
    if(pid > 0) 
    {
-      printf(1,"This has no child, start waiting\n");
-     
-
+      printf(1,"This Parent has a child, start waiting\n");      
+      printf(1,"Wait, %d\n", wait(0));
+      printf(1,"Parent finished\n");
+   }
+   else if(pid == 0)
+   {   
+      printf(1,"Child has started sleeping\n");      
+      sleep(300);
+      printf(1,"Child finished\n");
+   }
+   exit(0);
+}
